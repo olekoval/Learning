4f3893c44f558c0abb60e68b7ceb13f769816773
oleh.koval <oleh.koval@nszu.gov.ua>
2025-07-02 15:07:27 +0300

    Flask міграція змін до skill

--- Flask/resume/cv.py
+++ Flask/resume/cv.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # БД
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cv.db' # 3-ри слеша
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)        # Ініціалізація БД
+migrate = Migrate(app, db)  # Ініціалізація Flask-Migrate
 
 # Опис моделей
 class Skill(db.Model):
@@ -13,6 +15,8 @@ class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
     description = db.Column(db.Text)
+    reting = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean, default=True)
     
     def __repr__(self):
         return f"<Skill - {self.name}>"
@@ -39,5 +43,12 @@ if __name__ == '__main__':
 >>> app.app_context().push()      # Активуємо контекст Flask
 >>> db.create_all()
 з'явиться новий каталог "instance" з створенною БД
+
+МІГРАЦІЯ змін у моделях БД
+>set FLASK_APP=cv.py # Зробити тільки один раз
+>flask db init # Зробити тільки один раз (з'явиться папка migration)
+- Зробити зміни у моделі
+>flask db migrate -m "Add boolean is_active field to Skill" # створює нову міграцію
+>flask db upgrade # застосовує міграцію
 """
 
